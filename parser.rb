64a28514e7b7633beb5439b12e009db53ec8dd0b
Sean Schulte <sirsean@gmail.com>
2010-05-29 11:33:00 -0500

    You can download and parse files from MLB.
    This uses the GameDay datafeed that MLB's apps use. Right now it's rudimentary and only supports a few days of the Twins, but the pieces are there for significant expansion.

---
+++ parser.rb
@@ -0,0 +1,72 @@
+require 'rexml/document'
+require 'date'
+
+class BatterGameScore
+    attr_reader :name, :date
+    def initialize(date, elem)
+        @date = date
+        @name = elem.attributes['name']
+        @pos = elem.attributes['pos']
+        @ab = elem.attributes['ab'].to_i
+        @r = elem.attributes['r'].to_i
+        @bb = elem.attributes['bb'].to_i
+        @sf = elem.attributes['sf'].to_i
+        @h = elem.attributes['h'].to_i
+        @e = elem.attributes['e'].to_i
+        @d = elem.attributes['d'].to_i
+        @t = elem.attributes['t'].to_i
+        @hbp = elem.attributes['hbp'].to_i
+        @so = elem.attributes['so'].to_i
+        @hr = elem.attributes['hr'].to_i
+        @rbi = elem.attributes['rbi'].to_i
+        @sb = elem.attributes['sb'].to_i
+        @avg = elem.attributes['avg']
+
+        @s = (@h - @hr - @t - @d)
+    end
+
+    # (0.72xNIBB + 0.75xHBP + 0.90x1B + 0.92xRBOE + 1.24x2B + 1.56x3B + 1.95xHR) / PA
+    def woba
+        (((0.72 * @bb) + (0.75 * @hbp) + (0.90 * @s) + (1.24 * @d) + (1.56 * @t) + (1.95 * @hr)) / (@ab + @bb + @hbp))
+    end
+end
+
+def parse_date_from_filename(filename)
+    regex = /.*gid_(\d+)_(\d+)_(\d+)_.*/.match(filename)
+    Date.new(regex[1].to_i, regex[2].to_i, regex[3].to_i)
+end
+
+# get all the files in the downloads directory
+boxscores = Dir.glob("downloads/*.xml")
+
+batter_scores = {}
+boxscores.each{ |filename|
+    date = parse_date_from_filename(filename)
+    team_flag = 'home'
+    doc = REXML::Document.new(File.read(filename))
+    doc.elements.each("boxscore"){ |elem|
+        home_team_code = elem.attributes['home_team_code']
+        away_team_code = elem.attributes['away_team_code']
+        if (home_team_code == 'min')
+            team_flag = 'home'
+        elsif away_team_code == 'min'
+            team_flag = 'away'
+        end
+    }
+    doc.elements.collect("boxscore/batting[@team_flag='#{team_flag}']/batter"){ |elem|
+        BatterGameScore.new(date, elem)
+    }.each { |batter|
+        puts "Date: #{batter.date}; Batter: #{batter.name}, wOBA: #{batter.woba}"
+        if not batter_scores[batter.name]
+            batter_scores[batter.name] = []
+        end
+        batter_scores[batter.name].push(batter)
+    }
+}
+
+batter_scores.keys.each{|name|
+    scores = batter_scores[name]
+    scores.each{ |score|
+        puts "#{name}: #{score.date}, wOBA: #{score.woba}"
+    }
+}
